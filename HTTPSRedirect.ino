eea38623a11e25456701da650d01011b7af43dc3
electronicsguy <electronicsguy123@gmail.com>
2021-07-05 14:31:11 +0530

    Initial commit into new repo
    The previous location of this library was:
    https://github.com/electronicsguy/ESP8266/tree/master/HTTPSRedirect
    
    Since it was inside another folder, I've moved it into the home
    directory to make it a standlone repo.
    
    Includes github-ci, licence and badges.

---
+++ HTTPSRedirect.ino
@@ -0,0 +1,4 @@
+// The file exists only to satisfy the requirement of arduino-cli
+// https://github.com/arduino/arduino-cli/issues/765
+// It needs the main compiled file to have the same name as the repo
+
